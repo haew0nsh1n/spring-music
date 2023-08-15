44a26a8e7a45e28585f250e74d987cf95aac4b5d
haewons-tanzu <haewons@vmware.com>
2023-08-15 21:44:47 +0900

    commit

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -4,8 +4,13 @@ import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer
 import org.cloudfoundry.samples.music.repositories.AlbumRepositoryPopulator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @SpringBootApplication
+@Configuration
 public class Application {
 
     public static void main(String[] args) {
@@ -14,5 +19,13 @@ public class Application {
                 .listeners(new AlbumRepositoryPopulator())
                 .application()
                 .run(args);
+    }
+    
+    @Configuration
+    public static class DisableSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/**");
+        }
     }
 }
